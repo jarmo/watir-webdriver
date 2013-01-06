3b92370f3608e6fa911499d1b10d1dd175cfe4a1
Jarmo Pertman <jarmo.p@gmail.com>
2013-01-07 00:31:20 +0200

    Set PhantomJS as first into path to avoid already installed PhantomJS.

--- support/travis.sh
+++ support/travis.sh
@@ -17,6 +17,6 @@ fi
 if [[ "$WATIR_WEBDRIVER_BROWSER" = "phantomjs" ]]; then
   curl -L -O "http://phantomjs.googlecode.com/files/phantomjs-1.8.1-linux-i686.tar.bz2"
   bzip2 -cd phantomjs-1.8.1-linux-i686.tar.bz2 | tar xvf -
-  PATH=$PATH:$PWD/phantomjs-1.8.1-linux-i686/bin/
+  PATH=$PWD/phantomjs-1.8.1-linux-i686/bin/:$PATH
   chmod +x phantomjs-1.8.1-linux-i686/bin/phantomjs
 fi
