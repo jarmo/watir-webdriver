f9f602cc811be350bcc69de7db06109f11fdd94d
Jarmo Pertman <jarmo.p@gmail.com>
2013-01-25 22:12:14 +0200

    Use css locators for IE since it ought to be faster.

--- support/travis.sh
+++ support/travis.sh
@@ -30,5 +30,5 @@ if [[ "$WATIR_WEBDRIVER_BROWSER" = "internet_explorer" ]]; then
   unzip -d Sauce-Connect Sauce-Connect-latest.zip
   java -version
   java -jar Sauce-Connect/Sauce-Connect.jar $SAUCE_LABS_USER $SAUCE_LABS_ACCESS_KEY 1>sauce-connect.out &
-  while ! grep -q "Connected! You may start your tests." sauce-connect.out; do  sleep 1; done
+  while ! grep -q "Connected! You may start your tests." sauce-connect.out; do sleep 1; done
 fi
