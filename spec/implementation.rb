b076303a2cc0d846c69d1d8282270d33603db688
Jarmo Pertman <jarmo.p@gmail.com>
2013-02-20 23:57:37 +0200

    Add browser version to guards.

--- spec/implementation.rb
+++ spec/implementation.rb
@@ -67,28 +67,38 @@ class ImplementationConfig
 
   def set_guard_proc
     matching_browser = remote? ? remote_browser : browser
+    browser_instance = WatirSpec.new_browser
+    browser_version = browser_instance.driver.capabilities.version
+    matching_browser_with_version = "#{matching_browser}#{browser_version}".to_sym
     matching_guards = [
       :webdriver,                     # guard only applies to webdriver
       matching_browser,               # guard only applies to this browser
-      [:webdriver, matching_browser]  # guard only applies to this browser on webdriver
+      matching_browser_with_version,  # guard only applies to this browser with specific version
+      [:webdriver, matching_browser], # guard only applies to this browser on webdriver
+      [:webdriver, matching_browser_with_version]  # guard only applies to this browser with specific version on webdriver
     ]
 
     if native_events?
       # guard only applies to this browser on webdriver with native events enabled
       matching_guards << [:webdriver, matching_browser, :native_events]
+      matching_guards << [:webdriver, matching_browser_with_version, :native_events]
     else
       # guard only applies to this browser on webdriver with native events disabled
       matching_guards << [:webdriver, matching_browser, :synthesized_events]
+      matching_guards << [:webdriver, matching_browser_with_version, :synthesized_events]
     end
 
     if !Selenium::WebDriver::Platform.linux? || ENV['DESKTOP_SESSION']
       # some specs (i.e. Window#maximize) needs a window manager on linux
       matching_guards << [:webdriver, matching_browser, :window_manager]
+      matching_guards << [:webdriver, matching_browser_with_version, :window_manager]
     end
 
     @imp.guard_proc = lambda { |args|
       args.any? { |arg| matching_guards.include?(arg) }
     }
+  ensure
+    browser_instance.close
   end
 
   def firefox_args
@@ -146,7 +156,7 @@ class ImplementationConfig
   end
 
   def remote_browser
-    remote_browser = @imp.browser_class.new(*@imp.browser_args)
+    remote_browser = WatirSpec.new_browser
     remote_browser.browser.name
   ensure
     remote_browser.close
